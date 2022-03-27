fe2d6c2356aea24fb160170cd012d8d9f0e8c240
Hassan <hassan_99@live.dk>
2022-03-27 11:14:02 +0200

    new constant variables for configuration

--- src/settings/Settings.java
+++ src/settings/Settings.java
@@ -1,7 +1,17 @@
 package settings;
 
+import java.io.File;
+
 public class Settings {
 
     public static final int TESSERACT_DPI = 300;
+
+    public static final int ROBOT_DELAY = 500;
+    public static final int ROBOT_START_DELAY = 5000;
+    public static final int ROBOT_LEFT = 37;
+    public static final int ROBOT_RIGHT = 39;
+    public static final int ROBOT_DOWN = 40;
+
+    public static final File BOARD_IMAGE = new File("./resources/image/board.png");
 
 }
